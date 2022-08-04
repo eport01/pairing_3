4b076cbc21a87b30eb3ef21160ef317314ecdeb4
Emily Port <emilylport@gmail.com>
2022-08-04 14:34:38 -0600

    Add method

--- friends.rb
+++ friends.rb
@@ -3,4 +3,13 @@ numbers.each do |number|
   if number > 30
     puts number
   end
-end   
+end
+
+def under_60(variable)
+  variable.each do |number|
+    if number < 60
+      puts number
+    end
+  end
+end
+under_60(numbers)
